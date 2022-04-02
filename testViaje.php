5a1bc112c6a3e5cc5ca9e839f7b230ae18bbbcd9
zuccato3517 <93780909+zuccato3517@users.noreply.github.com>
2022-04-02 20:24:20 -0300

    Comentado.

--- testViaje.php
+++ testViaje.php
@@ -1,10 +1,15 @@
 
 <?php 
+
+//Incluyo el archivo que contiene la clase Viaje y declaro las variables a usar en el menú.
+
 include 'Viaje.php'; 
 
 $viaje = null;
 $opcion = 1;
 
+    //A continuación, el menú desde el cual eligiremos cada opción, implementada con switch.
+    //En cada caso que corresponda, no te permite utilizar la opción si no se cargó un vuelo.
 while($opcion != 0){
     $menu = "\033[1mMenú de opciones:\033[0m";
     echo "\033[4m$menu\033[0m";
@@ -19,7 +24,7 @@ while($opcion != 0){
     \n";
     $opcion = readline();
     switch($opcion){
-        case 0 : echo "Saliendo del test"; break;
+        case 0 : echo "Saliendo del menú.\n"; break;
         case 1 : $viaje = crearViaje();break;
         case 2 :  if ($viaje !== null) {agregarPasajero($viaje);} else {echo "No se cargó un viaje.\n";};break;
         case 3 : if ($viaje !== null) {modificarVuelo($viaje);} else {echo "No se cargó un viaje.\n";};break;
@@ -30,6 +35,29 @@ while($opcion != 0){
     }
 }
 
+    //Con crearViaje(), tomo los datos y creo un objeto Viaje.
+function crearViaje(){
+    $pasajeros = [];
+    $codigo= readline("Ingrese el codigo de vuelo: ");
+    $destino = readline("Ingrese el destino: ");
+    $capacidad = readline("Ingrese la capacidad del vuelo: ");
+    $nuevoViaje = new Viaje($codigo, $destino, $capacidad, $pasajeros);
+    echo "Se creo correctamente un nuevo viaje. \n";
+    return $nuevoViaje;
+  }
+
+  function agregarPasajero($nuevoViaje){
+    $nuevoViaje;
+    if (count($nuevoViaje->getColeccionPasajeros())+1<=$nuevoViaje->getCantidadPasajeros()){
+   $nombrePasajero = readline("Ingrese Nombre del Pasajero: ");
+   $apellidoPasajero = readline("Ingrese Apellido del pasajero: ");
+   $dniPasajero= readline("Ingrese Dni del pasajero: ");
+    return $nuevoViaje->populatePasajeros($nombrePasajero, $apellidoPasajero, $dniPasajero);
+} else {
+    echo "El vuelo está lleno.\n";
+}
+
+    //La siguiente opción abre un submenú para modificar los datos ingresados anteriormente sobre el vuelo:
 function modificarVuelo($viaje){
     $opcion = 1;
     while($opcion != 0){
@@ -45,7 +73,7 @@ function modificarVuelo($viaje){
         $opcion = readline();
         echo "\n";
         switch($opcion){
-            case 0 : echo "Saliendo del test"; break;
+            case 0 : echo "Saliendo del menú.\n"; break;
             case 1 : $cambiarCodigo = readline("Ingrese el nuevo codigo de vuelo: ");
                      $viaje->setCodigoVuelo($cambiarCodigo); break;
             case 2 : $cambiarDestino = readline("Ingrese el nuevo destino de vuelo: ");
@@ -56,26 +84,7 @@ function modificarVuelo($viaje){
     }                
 }
 
-function crearViaje(){
-  $pasajeros = [];
-  $codigo= readline("Ingrese el codigo de vuelo: ");
-  $destino = readline("Ingrese el destino: ");
-  $capacidad = readline("Ingrese la capacidad del vuelo: ");
-  $nuevoViaje = new Viaje($codigo, $destino, $capacidad, $pasajeros);
-  echo "Se creo correctamente un nuevo viaje. ";
-  return $nuevoViaje;
-}
 
-function agregarPasajero($nuevoViaje){
-    $nuevoViaje;
-    if (count($nuevoViaje->getColeccionPasajeros())+1<=$nuevoViaje->getCantidadPasajeros()){
-   $nombrePasajero = readline("Ingrese Nombre del Pasajero: ");
-   $apellidoPasajero = readline("Ingrese Apellido del pasajero: ");
-   $dniPasajero= readline("Ingrese Dni del pasajero: ");
-    return $nuevoViaje->populatePasajeros($nombrePasajero, $apellidoPasajero, $dniPasajero);
-} else {
-    echo "El vuelo está lleno.\n";
-}
 }
        
 ?>       
