932b8ceacd04e4458023366398abb53e1a94d992
zuccato3517 <93780909+zuccato3517@users.noreply.github.com>
2022-04-02 20:23:49 -0300

    Comentado.

--- Viaje.php
+++ Viaje.php
@@ -1,7 +1,7 @@
 <?php   
 class Viaje{
   
-  
+    //Declaro variables
     private $codigoVuelo;
     private $destino;
     private $cantPasajeros;
@@ -14,6 +14,8 @@ class Viaje{
         $this->cantPasajeros = $capacidad;
        
     }
+
+    //Declaro setters y getters genéricos
     public function getCodigoVuelo(){
         return $this->codigoVuelo;
     }
@@ -39,6 +41,9 @@ class Viaje{
         $this->coleccion_pasajeros[count($this->coleccion_pasajeros)] = $pasajero;
     }
 
+
+    //A la hora de agregar un pasajero, uso la siguiente función para que me de el formato
+    //correcto al agregar cada pasajero al arreglo de colección:
     public function populatePasajeros($nombre, $apellido, $dni) {  
         $pasajero=array(
            
@@ -49,22 +54,31 @@ class Viaje{
         $this->setPasajero($pasajero);
     } 
 
+    //La siguiente función corresponde a la opción 4 del menú:
     function modificarPasajero($DNIPasajero){
+        //Primero checkeo que haya algún pasajero que editar.
         if(count($this->coleccion_pasajeros)>0){
+        
+            //Creo una variable auxiliar, que se actualizará si encuentra el DNI del pasajero:
         $aux = "none";
         $arregloSalida = $this->coleccion_pasajeros;
         foreach ($arregloSalida as $key => $subArr)
         {
+            //Si encuentra que un DNI de un subarray corresponde con el DNI que le pasé...
             if ($subArr["dni"] == $DNIPasajero)
             {
+                //...guarda la llave en el auxiliar, y remueve la entrada.
             $aux = $key;
             unset($arregloSalida[$key]);
             }
         }
+            //Caso contrario, informará que no existe tal pasajero y sale de la función.
         if ($aux == "none"){
             echo "Ese pasajero no existe.\n";
             return;
         }
+
+            //Una vez encontrado el pasajero, reingresamos los datos.
         $nombrePasajero = readline("Ingrese Nombre del Pasajero: ");
         $apellidoPasajero = readline("Ingrese Apellido del pasajero: ");
         $dniPasajero= readline("Ingrese Dni del pasajero: ");
@@ -76,27 +90,31 @@ class Viaje{
        );
         $this->coleccion_pasajeros[$aux] = $pasajero;
     }else{
-        echo "No cargaste ningun pasajero en el viaje.";
+            //Y si no había pasajeros, nos lo informa.
+        echo "No cargaste ningun pasajero en el viaje.\n";
     }
    }
 
+   //Para mantener más limpio el __toString, armé una función que concatena cada pasajero con sus datos.
    public function datosPasajeros(){
     $aux = "";
+    $i=1;
     $arregloSalida = $this->coleccion_pasajeros;
     foreach ($arregloSalida as $key => $subArr)
     {
-        $aux = $aux."Nombre: ".$subArr["nombre"]."\nApellido: ".$subArr["apellido"]."\nDNI: ".$subArr["dni"].".\n";
+        $aux = $aux.$i.":\nNombre: ".$subArr["nombre"]."\nApellido: ".$subArr["apellido"]."\nDNI: ".$subArr["dni"].".\n";
+        $i++;
     }
     return $aux;
    }
 
+        //La función anterior irá concatenada al final de los datos del vuelo, detallando los pasajeros.
+
    public function __toString()
    {
-       return "Los datos del viaje son: \nCódigo: ".$this->getCodigoVuelo().", Destino: ".$this->getDestino().", Cantidad de pasajeros: ".$this->getCantidadPasajeros().".\n".$this->datosPasajeros();
+       return "Los datos del viaje son: \nCódigo: ".$this->getCodigoVuelo().", Destino: ".$this->getDestino().", Cantidad máxima de pasajeros: ".$this->getCantidadPasajeros().".\n".$this->datosPasajeros();
    }
 
 }
-
-
 
 ?> 
