16cccb613352cade566f747c99801543fdf94ec9
zuccato3517 <93780909+zuccato3517@users.noreply.github.com>
2022-05-16 16:10:04 -0300

    Update Viaje.php

--- Viaje.php
+++ Viaje.php
@@ -2,25 +2,32 @@
 class Viaje{
   
   
-    private $codigoVuelo;
+    private $codigoViaje;
     private $destino;
     private $cantPasajeros;
+    private $cantMaxPasajeros;
     private $coleccion_pasajeros= []; //es un arreglo de Objetos Pasajero
     private $responsable; //es un Objeto Responsable V
+    private $importe;
+    private $tramos;//ida y vuelta
+
 
     //Clase constructora con valores
-    public function __construct($codigo, $desti, $capacidad,$colPasajeros,$responsable ){
-        $this->codigoVuelo = $codigo;
+    public function __construct($codigo, $desti, $cantidad, $cantidadMax,$colPasajeros,$esResponsable,$esImporte, $esTramos ){
+        $this->codigoViaje = $codigo;
         $this->destino = $desti;
-        $this->cantPasajeros = $capacidad;
+        $this->cantidad = $cantidad;
+        $this->cantMaxPasajeros = $cantidadMax;
         $this->coleccion_pasajeros = $colPasajeros;
-        $this->responsable=$responsable;
+        $this->responsable=$esResponsable;
+        $this->importe = $esImporte;
+        $this->tramos=$esTramos;
 
        
     }
     //Get`s de los atributos
     public function getCodigoVuelo(){
-        return $this->codigoVuelo;
+        return $this->codigoViaje;
     }
     public function getDestino(){
         return $this->destino;
@@ -28,15 +35,24 @@ class Viaje{
     public function getCantidadPasajeros(){
         return $this->cantPasajeros;
     }    
+    public function getCantidadMaxPasajeros(){
+        return $this->cantMaxPasajeros;
+    }    
     public function getColeccionPasajeros(){
         return $this->coleccion_pasajeros;
     }
     public function getResponsable(){
         return $this->responsable;
     }
+    public function getImporte(){
+        return $this->importe;
+    }
+    public function getTramos(){
+        return $this->tramos;
+    }
     //Set`s de los atributos
     public function setCodigoVuelo($nuevoCodigo){
-         $this->codigoVuelo = $nuevoCodigo;
+         $this->codigoViaje = $nuevoCodigo;
     }
     public function setDestino($nuevoDestino){
           $this->destino = $nuevoDestino;
@@ -44,18 +60,36 @@ class Viaje{
     public function setCantidadPasajeros($nuevaCantidad){
          $this->cantPasajeros = $nuevaCantidad;
     }   
+    public function setCantidadMaxPasajeros($nuevaCantidad){
+        $this->cantMaxPasajeros = $nuevaCantidad;
+   }   
     public function setColeccionPasajeros($nuevaColeccion){
         $this->coleccion_pasajeros = $nuevaColeccion;
    } 
      public function setResponsable($nuevoResponsable){
          $this->responsable= $nuevoResponsable;
      }
+     public function setImporte($nuevoImporte){
+        $this->importe = $nuevoImporte;
+   }
+   public function setTramo($nuevoTramo){
+         $this->Tramo = $nuevoTramo;
+   } 
 
+   public function hayPasajesDisponible(){
+    $hayPasajes=false;
+   echo $A= count($this->getColeccionPasajeros());
+   var_dump($this->getColeccionPasajeros());
+        if  ($A <$this->getCantidadMaxPasajeros()){
+        $hayPasajes=true;
+    }
+    return $hayPasajes;
+}
 
    public function agregarPasajeroAColeccion($unPasajero){ //recibe por parametro el new pasajero creado en testViaje
    
     $array_pasajeros = $this->getColeccionPasajeros(); //Trae la coleccion de pasajeros
-     $largo=count($array_pasajeros);   //cuenta el largo de la coleccion de pasajeros
+    $largo=count($array_pasajeros);   //cuenta el largo de la coleccion de pasajeros
     $i=0; 
     $encontro=false;
     $dniPasajero=$unPasajero->getDNI(); //selecciona SOLO el dni del pasajero
@@ -74,144 +108,81 @@ class Viaje{
                 $coleccion=$this->getColeccionPasajeros(); //trae a la coleccion pasajeros
                 array_push($coleccion,$unPasajero); //agregar el Pasajero
                $this->setColeccionPasajeros($coleccion);// Set la coleccion con el pasajero nuevo
+               $this->setCantidadPasajeros($this->getCantidadPasajeros()+1);
             }
         }
-    
   }
 
-
-   public function cambiarPasajero($dni){  //iba viaje en parametro?
-    $esCorrecto=false; $encontro=false;
-    $op=1; $posicion=-1; $i=0;
+  public function encontrarPasajero($dni){
+    $esElPasajero= -1;
+    $posicion=-1;
+    $i=0;
+    $encontro=false;
     $array_pasajeros = $this->getColeccionPasajeros();
     $largo=count($array_pasajeros);
+    while ($i<$largo && !$encontro) {  //mientras que $i sea Menor que Tamaño arreglo Y $encontro= false
+    $pasajero = $array_pasajeros[$i]; //pasajero = EL OBJETO PASAJERO en posicion $i del array.
+
+  if ($pasajero->getDNI()==$dni) { //SI el N de documento de $Pasajero es Igual a $dni que puso usuario
+
+     $encontro=true;
+      $posicion=$i; //guardamos la posicion!!! LA USAMOS DESPUES.
+      $esElPasajero= $posicion;
+      echo $esElPasajero;
+
+  }
+        $i++;//Si NO es igual, $i+1 y de nuevo el bucle
+    }
+    return $esElPasajero;
+}
+
+
+
+public function modificarPasajero($esElPasajero, $op, $datoIngresado) {
+    $array_pasajeros = $this->getColeccionPasajeros();
     
-    // if($largo>0){ //confirma que arreglo existe. Es Mayor a 0
-    //     while(!$esCorrecto){  //Mientras que $esCorrecto= false, sigue este bucle.
-    //         $dni = readline("Ingrese el DNI para modificar el pasajero");
-    //         if(is_numeric($dni)){ //confirma que usuario ingreso un NUMERO 
-    //             $esCorrecto=true; //si ingreso un numero sale del bucle y va al siguiente
-    //         }else{
-    //             echo "ERROR: Debe ingresar un numero\n"; //si el usuario ingreso el dato mal, lo pide otra VEZ
-    //         }
-    //     }
-        
-        //Encuentro la posicion del Pasajero
-        while($i<$largo && !$encontro){  //mientras que $i sea Menor que Tamaño arreglo Y $encontro= false
-              $pasajero = $array_pasajeros[$i]; //pasajero = EL OBJETO PASAJERO en posicion $i del array.
-            if($pasajero->getDNI()==$dni){ //SI el N de documento de $Pasajero es Igual a $dni que puso usuario
-               $encontro=true; 
-               $posicion=$i; //guardamos la posicion!!! LA USAMOS DESPUES.
-            }
-            $i++;//Si NO es igual, $i+1 y de nuevo el bucle
-        }
-        if($encontro){ //Si lo encontro
-            
-                while($op != 0){
-                    $esCorrecto=false;
-                    echo  
-                     "Ingrese que desea modificar del pasajero
-                      0- Salir
-                      1- Nombre del pasajero
-                      2- Apellido del pasajero
-                      3- Telefono\n";
+    if ($op == 1) {
+        $pasajero=$array_pasajeros[$esElPasajero]; //pasajero es el Objeto del array en $posicion ($i)
+        $pasajero->setNombre($datoIngresado); //Usamos Set con el nuevo nombre
+        $array_pasajeros[$esElPasajero] = $pasajero;//seteamos el nuevopasajero en el arreglo
+        $this->setColeccionPasajeros($array_pasajeros);
+    }
+    elseif ($op= 2) {
+       $pasajero=$array_pasajeros[$esElPasajero]; //pasajero es el Objeto del array en $posicion ($i)
+       $pasajero->setApellido($datoIngresado); //Usamos Set con el nuevo nombre
+       $array_pasajeros[$esElPasajero] = $pasajero;//seteamos el nuevopasajero en el arreglo
+       $this->setColeccionPasajeros($array_pasajeros);
+   }
+    elseif ($op=3){
+       $pasajero=$array_pasajeros[$esElPasajero]; //pasajero es el Objeto del array en $posicion ($i)
+       $pasajero->setTelefono($datoIngresado); //Usamos Set con el nuevo nombre
+       $array_pasajeros[$esElPasajero] = $pasajero;//seteamos el nuevopasajero en el arreglo
+       $this->setColeccionPasajeros($array_pasajeros);
+    }
 
-                $op = readline(); //lee que pone usuario
-                switch($op){
-                    case 1 : 
-                        $nombrePasajero=readline("Ingrese nuevo Nombre");//guardamos en $nombrePasajero el Nuevo nombre
-
-                        $pasajero=$array_pasajeros[$posicion]; //pasajero es el Objeto del array en $posicion ($i)
-                        $pasajero->setNombre($nombrePasajero); //Usamos Set con el nuevo nombre
-                        $array_pasajeros[$posicion] = $pasajero;//seteamos el nuevopasajero en el arreglo
-                        $this->setColeccionPasajeros($array_pasajeros);//seteamos el arreglo modificado 
-                        break;
-                    case 2 : 
-                        $apellidoPasajero=readline("Ingrese nuevo Apellido");
-                        $pasajero = $array_pasajeros[$posicion];
-                        $pasajero->setApellido($apellidoPasajero);
-                        $array_pasajeros[$posicion]=$pasajero;
-
-                        $this->setColeccionPasajeros($array_pasajeros);
-                        break;
-                    case 3 : 
-                        $apellidoPasajero=readline("Ingrese nuevo telefono");
-                        $pasajero = $array_pasajeros[$posicion];
-                        $pasajero->setTelefono($apellidoPasajero);
-                        $array_pasajeros[$posicion]=$pasajero;
-
-                        $this->setColecccionPasajeros($array_pasajeros);
-                        break;
-                        
-                       
-                    default: echo"ERROR Opcion incorrecta. Ingrese 0-1-2-3.\n"; break; //solo permite que usuario ingrese opcion valida
-                   }
-                }
-            }
-            else
-                echo "ERROR No se encontro pasajero con ese DNI.\n";
-        
-      
 }
 
-//Funcion modificar vuelo, Un menu, permite Modificar individualmente el Codigo de vuelo, destino y la cantidad maxima de pasajeros.
-function modificarVuelo(){
-    $opcion = 1;
-    while($opcion != 0){
-        echo "
-        \n
-        0-Salir 
-        1-Modificar Codigo de vuelo 
-        2-Modificar Destino
-        3-Modificar Cantidad Maxima Pasajeros
-        \n";
-        $opcion = readline();
-        echo "\n";
-        switch($opcion){
-            case 0 : echo "Saliendo del test"; break;
-            case 1 : $cambiarCodigo = readline("Ingrese el nuevo codigo de vuelo: ");
-                     $this->setCodigoVuelo($cambiarCodigo); break;
-            case 2 : $cambiarDestino = readline("Ingrese el nuevo destino de vuelo: ");
-                     $this->setDestino($cambiarDestino); break;
-            case 3 :  $cambiarCantidad = readline("Ingrese la nueva cantidad maxima de pasajeros: ");
-                     $this->setCantidadPasajeros($cambiarCantidad); break;
-                    }
-    }                
-}
-
-public function modificarResponsable(){
+public function modificarResponsable( $op, $datoIngresado){
     $responsable=$this->getResponsable();
-    echo  
-                     "Ingrese que desea modificar del Responsable
-                      0- Salir
-                      1- Nombre del Responsable
-                      2- Apellido del Responsable
-                      3- Numero Licencia
-                      4- Numero Empleado\n";
 
-                $op = readline(); //lee que pone usuario
-                switch($op){
-                    case 1 : 
-                        $nombreResponsable=readline("Ingrese nuevo Nombre");//guardamos en $nombrePasajero el Nuevo nombre
-                        $responsable->setNombre($nombreResponsable); //Usamos Set con el nuevo nombre
-                         break;
-                    case 2 : 
-                        $apellidoResponsable=readline("Ingrese nuevo Apellido");//guardamos en $nombrePasajero el Nuevo nombre
-                        $responsable->setApellido($apellidoResponsable); //Usamos Set con el nuevo nombre
-                         break;
-                    case 3 : 
-                        $numeroLicencia=readline("Ingrese nuevo Numero de Licencia");//guardamos en $nombrePasajero el Nuevo nombre
-                        $responsable->setNumeroLicencia($numeroLicencia); //Usamos Set con el nuevo nombre
-                         break;
-                    case 4 :  
-                        $numeroEmpleado=readline("Ingrese nuevo Numero de Empleado");//guardamos en $nombrePasajero el Nuevo nombre
-                        $responsable->setNumeroEmpleado($numeroEmpleado); //Usamos Set con el nuevo nombre
-                         break;   
-                        
-                       
-                    default: echo"ERROR Opcion incorrecta. Ingrese 0-1-2-3-4.\n"; break;
+    if ($op == 1){
+        $responsable->setNombre($datoIngresado); //Usamos Set con el nuevo nombre
+    }
+    elseif ($op == 2) {
+        $responsable->setApellido($datoIngresado);
+    }
+    elseif ( $op == 3){
+
+        $responsable->setNumeroLicencia($datoIngresado);
+
+    }
+    elseif ($op ==4){
+        $responsable->setNumeroEmpleado($datoIngresado);
+    }
+    $this->setResponsable($responsable);
+
 }
-}
+
 public function imprimirPasajeros(){
  $listaPasajeros=$this->getColeccionPasajeros();
  $largo= count($listaPasajeros);
@@ -231,8 +202,9 @@ public function imprimirPasajeros(){
 
 public function __toString()
    {
-       return "Los datos viaje son: \nCodigo: ".$this->getCodigoVuelo().",\n Destino: ".$this->getDestino().", \nCantidad maxima de Pasajeros: ".$this->getCantidadPasajeros().
-       "\nResponsable:\n".$this->getResponsable();//"\nPasajeros:\n".$this->getColeccionPasajeros();
+      $cadena="Los datos viaje son: \nCodigo: ".$this->getCodigoVuelo().",\n Destino: ".$this->getDestino().", \nCantidad maxima de Pasajeros: ".$this->getCantidadMaxPasajeros().
+       "\nResponsable:".$this->getResponsable()."\nPasajeros: \n".$this->imprimirPasajeros()."\nImporte:\n".$this->getImporte().".";
+       return $cadena;
    }
 
 } 
